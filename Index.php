23fd604a6949e93aa00d3ae048f6e246b857052e
IvarHuizing <ivar.huizing@student.stenden.com>
2018-07-10 13:16:05 +0200

    change text, remove links and settings

--- Index.php
+++ Index.php
@@ -1,3 +1,4 @@
+
 <?php 
 require_once('function_login.php');
 ?>
@@ -6,7 +7,7 @@ require_once('function_login.php');
 <head>
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0"/>
-  <title>Starter Template - Materialize</title>
+  <title>SmartProjects</title>
 
   <!-- CSS  -->
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
@@ -15,9 +16,9 @@ require_once('function_login.php');
 </head>
 <body>
   <nav class="light-blue lighten-1" role="navigation">
-    <div class="nav-wrapper container"><a id="logo-container" href="#" class="brand-logo">Logo</a>
+    <div class="nav-wrapper container"><a id="logo-container" href="#" class="brand-logo">SmartProject</a>
       <ul class="right hide-on-med-and-down">
-        <li><a href="#">Navbar Link</a></li>
+        <li><a href="#">Inloggen</a></li>
       </ul>
 
       <ul id="nav-mobile" class="sidenav">
@@ -29,12 +30,12 @@ require_once('function_login.php');
   <div class="section no-pad-bot" id="index-banner">
     <div class="container">
       <br><br>
-      <h1 class="header center orange-text">Starter Template</h1>
+      <h1 class="header center orange-text">SmartProjects</h1>
       <div class="row center">
-        <h5 class="header col s12 light">A modern responsive front-end framework based on Material Design</h5>
+        <h5 class="header col s12 light">Maakt een einde aan slechtlopende projecten</h5>
       </div>
       <div class="row center">
-        <a href="http://materializecss.com/getting-started.html" id="download-button" class="btn-large waves-effect waves-light orange">Get Started</a>
+        <a href="index.php" id="download-button" class="btn-large waves-effect waves-light orange">Maak een account</a>
       </div>
       <br><br>
 
@@ -50,27 +51,27 @@ require_once('function_login.php');
         <div class="col s12 m4">
           <div class="icon-block">
             <h2 class="center light-blue-text"><i class="material-icons">flash_on</i></h2>
-            <h5 class="center">Speeds up development</h5>
+            <h5 class="center">Geeft het project een boost</h5>
 
-            <p class="light">We did most of the heavy lifting for you to provide a default stylings that incorporate our custom components. Additionally, we refined animations and transitions to provide a smoother experience for developers.</p>
+            <p class="light">Projecten die niet goed lopen of op te leveren bestanden die niet goed zijn of te laat zijn ingeleverd zijn vanaf nu verleden tijd met SmartProjects, waar je in een oog opslag de belangrijkste zaken van het project ziet.</p>
           </div>
         </div>
 
         <div class="col s12 m4">
           <div class="icon-block">
             <h2 class="center light-blue-text"><i class="material-icons">group</i></h2>
-            <h5 class="center">User Experience Focused</h5>
+            <h5 class="center">Gericht op het projectgroep</h5>
 
-            <p class="light">By utilizing elements and principles of Material Design, we were able to create a framework that incorporates components and animations that provide more feedback to users. Additionally, a single underlying responsive system across all platforms allow for a more unified user experience.</p>
+            <p class="light">Na het selecteren van de juiste projectgroep kan het samenwerken beginnen en kun je zien welke to-do's er zijn en wie daar mee bezig is. Ook kun je zien welke onderdelen al klaar zijn.</p>
           </div>
         </div>
 
         <div class="col s12 m4">
           <div class="icon-block">
             <h2 class="center light-blue-text"><i class="material-icons">settings</i></h2>
-            <h5 class="center">Easy to work with</h5>
+            <h5 class="center">Makkelijk te gebruiken</h5>
 
-            <p class="light">We have provided detailed documentation as well as specific code examples to help new users get started. We are also always open to feedback and can answer any questions a user may have about Materialize.</p>
+            <p class="light">Na het maken van een account selecteer je de desbetreffende projectgroep en vervolgens zie je in een opslag de to-do lijst, deadlines en tips om valkuilen te voorkomen.</p>
           </div>
         </div>
       </div>
@@ -84,33 +85,15 @@ require_once('function_login.php');
       <div class="row">
         <div class="col l6 s12">
           <h5 class="white-text">Company Bio</h5>
-          <p class="grey-text text-lighten-4">We are a team of college students working on this project like it's our full time job. Any amount would help support and continue development on this project and is greatly appreciated.</p>
+          <p class="grey-text text-lighten-4">We zijn INF1A, oftewel projectgroep Informatica jaar 1 periode 4, van NHL Stenden Hogeschool Emmen en hebben deze website gemaakt voor project Innovate.</p>
 
 
-        </div>
-        <div class="col l3 s12">
-          <h5 class="white-text">Settings</h5>
-          <ul>
-            <li><a class="white-text" href="#!">Link 1</a></li>
-            <li><a class="white-text" href="#!">Link 2</a></li>
-            <li><a class="white-text" href="#!">Link 3</a></li>
-            <li><a class="white-text" href="#!">Link 4</a></li>
-          </ul>
-        </div>
-        <div class="col l3 s12">
-          <h5 class="white-text">Connect</h5>
-          <ul>
-            <li><a class="white-text" href="#!">Link 1</a></li>
-            <li><a class="white-text" href="#!">Link 2</a></li>
-            <li><a class="white-text" href="#!">Link 3</a></li>
-            <li><a class="white-text" href="#!">Link 4</a></li>
-          </ul>
         </div>
       </div>
     </div>
     <div class="footer-copyright">
       <div class="container">
-      Made by <a class="orange-text text-lighten-3" href="http://materializecss.com">Materialize</a>
+    
       </div>
     </div>
   </footer>
