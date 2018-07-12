baaccd241c221a1d2bf5a65687cec8b7de5e803e
IvarHuizing <ivar.huizing@student.stenden.com>
2018-07-12 21:36:27 +0200

    Add improved sidebar & start content part

--- home.php
+++ home.php
@@ -10,62 +10,131 @@
   <link href="css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
   <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
 </head>
-<body>
+<body class="loaded">
+    <header id="header" class="page-topbar">
     
   <nav class="light-blue lighten-1" role="navigation">
     <div class="nav-wrapper container"><a id="logo-container" href="home.php" class="brand-logo">SmartProject</a>
       <ul class="right hide-on-med-and-down">
         <li><a href="Index.php">Uitloggen</a></li>
       </ul>
-
-      <ul id="nav-mobile" class="sidenav">
-        <li><a href="#">Navbar Link</a></li>
-      </ul>
-      <a href="#" data-target="nav-mobile" class="sidenav-trigger"><i class="material-icons">menu</i></a>
     </div>
   </nav>
-    
-  <div class="section no-pad-bot" id="index-banner">
-        </div>
-    <ul id="nav-mobile" class="sidenav sidenav-fixed" style="transform: translateX(0%);">
-        <li class="bold"><a href="#" class="waves-effect waves-teal">About</a></li>
-        
-      </ul>
-     
-   
-    
-    
-    <div class="container">
-        
-        
-        
-        
-      <br><br>
-     
-      <div class="row center">
-
-      </div>
-      <div class="row center">
-
-      </div>
-      <br><br>
-
-    </div>
-    
-    
-  </div>
-
-
-  <div class="container">
-    <div class="section">
-
-      <!--   Icon Section   -->
+        </header>
+    <div id="main">
+      <!-- START WRAPPER -->
+      <div class="wrapper">
+        <!-- START LEFT SIDEBAR NAV-->
+    <aside id="left-sidebar-nav">
+          <li class="no-padding">
+              <ul class="collapsible" data-collapsible="accordion">
+                <li class="bold active">
+                  <a class="collapsible-header waves-effect waves-cyan active">
+                    <i class="material-icons">dashboard</i>
+                    <span class="nav-text">Jaar 1</span>
+                  </a>
+                  <div class="collapsible-body" style="display: block;">
+                    <ul>
+                      <li>
+                        <a href="#">
+                          <i class="material-icons">keyboard_arrow_right</i>
+                          <span>periode 1</span>
+                        </a>
+                      </li>
+                      <li class="active">
+                        <a href="#">
+                          <i class="material-icons">keyboard_arrow_right</i>
+                          <span>periode 2</span>
+                        </a>
+                      </li>
+                      <li class="active">
+                        <a href="#">
+                          <i class="material-icons">keyboard_arrow_right</i>
+                          <span>periode 3</span>
+                        </a>
+                      </li>
+                      <li class="active">
+                        <a href="#">
+                          <i class="material-icons">keyboard_arrow_right</i>
+                          <span>periode 4</span>
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                </li>
+                <li class="bold">
+                  <a class="collapsible-header waves-effect waves-cyan">
+                    <i class="material-icons">dvr</i>
+                    <span class="nav-text">Jaar 2</span>
+                  </a>
+                  <div class="collapsible-body" style="display:block;">
+                    <ul>
+                      <li>
+                        <a href="#">
+                          <i class="material-icons">keyboard_arrow_right</i>
+                          <span>periode 1</span>
+                        </a>
+                      </li>
+                      <li class="active">
+                        <a href="#">
+                          <i class="material-icons">keyboard_arrow_right</i>
+                          <span>periode 2</span>
+                        </a>
+                      </li>
+                      <li class="active">
+                        <a href="#">
+                          <i class="material-icons">keyboard_arrow_right</i>
+                          <span>periode 3</span>
+                        </a>
+                      </li>
+                      <li class="active">
+                        <a href="#">
+                          <i class="material-icons">keyboard_arrow_right</i>
+                          <span>periode 4</span>
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                </li>
+                <li class="bold">
+                  <a class="collapsible-header waves-effect waves-cyan">
+                    <i class="material-icons">web</i>
+                    <span class="nav-text">Jaar 3</span>
+                  </a>
+                  <div class="collapsible-body"style="display:block;">
+                    <ul>
+                      <li>
+                        <a href="#">
+                          <i class="material-icons">keyboard_arrow_right</i>
+                          <span>periode 3</span>
+                        </a>
+                      </li>
+                      <li>
+                        <a href="#">
+                          <i class="material-icons">keyboard_arrow_right</i>
+                          <span>periode 4</span>
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                </li>
+              </ul>
+              </li>
+        </aside>
       
-
-    </div>
-    <br><br>
-  </div>
-
+        <!-- END LEFT SIDEBAR NAV-->
+        <!-- //////////////////////////////////////////////////////////////////////////// -->
+        <!--START CONTENT-->
+        <section id="content">
+          <!--start container-->
+          <div class="container">
+          </div>
+          <!--end container-->
+        </section>
+        </div>
+       
+        <!--END CONTENT-->
+        
   <footer class="page-footer orange">
     <div class="container">
       <div class="row">
@@ -86,7 +155,8 @@
   <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
   <script src="js/materialize.js"></script>
   <script src="js/init.js"></script>
-
+      
+    </div>
   </body>
 </html>
 
