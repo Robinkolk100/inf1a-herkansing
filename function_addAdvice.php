07f20c2836ee4f015605f543c761cc98b7dda34c
WackyRage <a.m.westerhof@gmail.com>
2018-07-12 09:45:55 +0200

    create advice function

---
+++ function_addAdvice.php
@@ -0,0 +1,61 @@
+<?php
+
+	function addAdvice($documentID, $adviceType, $adviceTekst)
+	{
+		$count = 0;
+		$errorCount = 0;
+		$errorArray = array();
+
+		if (!ctype_digit(strval($documentID))) {
+        	array_push($errorArray, "Error on document selection");
+    	}
+
+    	$errorCount = count($errorArray);
+    	if($errorCount == 0)
+    	{
+    		if($count == 0)
+    		{
+    			$conn = dbConnect();
+    			$result = $conn->query(" SELECT * FROM `documents` 
+                                        WHERE `documentID`='".$documentID."';");
+
+            	$count = $result->num_rows;
+    		}
+
+    		if($count > 0)
+    		{
+    			$sql = "INSERT INTO `advice` (`adviceID`, `documentID`, `adviceType`, `adviceTekst`) 
+    					VALUES (NULL, '".$documentID."', '".."', '".."');";
+
+    			if ($conn->query($sql) === true) 
+    			{
+    				array_push($errorArray, "New record created successfully<br>");
+    			}
+    			else 
+    			{
+    				echo "Error: " . $sql . "<br>" . $conn->error;
+    			}
+
+    			$result->close();
+    			$conn->close();
+            	array_push($errorArray, "is gelukt<br>");
+
+            	return $errorArray;
+    		}
+    		else 
+    		{
+           	 	/* close connection */
+            	$conn->close();
+          	 	// header('Location:login.php?status=fail');
+            	array_push($errorArray, "FAIL is al eerder opgegeven<br>");
+            	return $errorArray;
+        	}
+    	}
+    	else 
+    	{
+        	return $errorArray;
+    	}
+
+	}
+
+?>
