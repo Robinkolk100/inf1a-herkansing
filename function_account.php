2c18430bc05e9372430356947b8d1f3df1a91d25
WackyRage <a.m.westerhof@gmail.com>
2018-07-13 11:22:55 +0200

    added commentline, ordering code, making correct return responses

--- function_account.php
+++ function_account.php
@@ -6,28 +6,33 @@ function NewAccountUser($student_ID, $student_userEmail, $student_userPass)
     $message = array();
 
     //Validate E-mail
-    if (!filter_var($student_userEmail, FILTER_VALIDATE_EMAIL)) {
+    if (!filter_var($student_userEmail, FILTER_VALIDATE_EMAIL)) 
+    {
         array_push($message, "<span class='card-title red-text text-accent-4'><h3>Geen valid Email is opgegeven</span></h3>");
     }
     
     // Check if variable is an integer
-    if (!ctype_digit(strval($student_ID))) {
+    if (!ctype_digit(strval($student_ID))) 
+    {
         array_push($message, "<span class='card-title red-text text-accent-4'><h3>Opgegeven variable is geen nummer </h3></span>");
     }
 
     $errcount = count($message);
-    if ($errcount === 0) {
+    if ($errcount === 0) 
+    {
         // kijken als deze persoon al eerder heeft opgegeven
-        if ($count == 0) {
+        if ($count == 0) 
+        {
             $conn = dbConnect();
             $result = $conn->query("SELECT `userName`, `userEmail` FROM `users` WHERE `userID` = '" . $student_ID . "' OR `userEmail` = '" . $student_userEmail . "';");
 
-            // Mysql_num_row is counting table row
+            // Mysqli_num_row is counting table row
             $count = $result->num_rows;
         }
 
         // persoon in database zetten
-        if ($count == 0) {
+        if ($count == 0) 
+        {
             $userName = strstr($student_userEmail, '@', true); // user name
             $userName = str_replace('.', ' ', $userName); // replace dot met black space 
             $student_userPass = mysqli_real_escape_string($conn, $student_userPass);
@@ -35,11 +40,14 @@ function NewAccountUser($student_ID, $student_userEmail, $student_userPass)
 
             $sql = "INSERT INTO `users` (`userID`, `userName`, `userEmail`, `userPass`) VALUES ('" . $student_ID . "', '" . $userName . "', '" . $student_userEmail . "', '" . $userPass_encrypt . "')";
 
-            if ($conn->query($sql) === true) {
+            if ($conn->query($sql) === true) 
+            {
                 array_push($message, "<span class='card-title lime-text text-accent-4'><h3>Nieuwe gebruiker is toegevoegd</span></h3>");
                 $_SESSION["userID"] = $student_ID;
                 $_SESSION["login"] = 1;
-            } else {
+            } 
+            else 
+            {
                 echo "<span class='card-title red-text text-accent-4'>  Error: " . $sql . "</span><br>" . $conn->error;
             }
             /* close result set */
@@ -50,9 +58,8 @@ function NewAccountUser($student_ID, $student_userEmail, $student_userPass)
             //header('Location:login.php?status=succes');
             return $message;
         }
-        
-        //als count niet 0 is heeft deze persoon zich al eerder opgegeven
-        else {
+        else 
+        {
             /* close connection */
             $conn->close();
             // als wachtwoord of nummer niet kloppen 
@@ -60,7 +67,9 @@ function NewAccountUser($student_ID, $student_userEmail, $student_userPass)
             return $message;
         }
 
-    } else {
+    } 
+    else 
+    {
         return $message;
     }
 }
--- function_addProjectDocument.php
+++ function_addProjectDocument.php
@@ -6,26 +6,33 @@
 		$errorCount = 0;
 		$errorArray = array();
 
-		if (!ctype_digit(strval($documentID))) {
-        	array_push($errorArray, "Error on document selection");
+        // Check if variable is an integer
+		if (!ctype_digit(strval($documentID))) 
+        {
+        	array_push($errorArray, "<span class='card-title red-text text-accent-4'><h3>Er is iets fout gegaan bij het document selecteren.</span></h3>");
     	}
 
-    	if (!ctype_digit(strval($projectID))) {
-        	array_push($errorArray, "Error on project selection");
+        // Check if variable is an integer
+    	if (!ctype_digit(strval($projectID))) 
+        {
+        	array_push($errorArray, "<span class='card-title red-text text-accent-4'><h3>Er is iets fout gegaan bij het project selecteren.</span></h3>");
     	}
 
     	$errorCount = count($errorArray);
     	if($errorCount == 0)
     	{
+            // check if document is already linked
     		if($count == 0)
     		{
     			$conn = dbConnect();
     			$result = $conn->query(" SELECT * FROM `projectdocuments` 
                                         WHERE `projectID`='".$projectID."' AND `documentID`='".$documentID."'");
 
+                // Mysqli_num_row is counting table row
             	$count = $result->num_rows;
     		}
 
+            //Adding document to project
     		if($count == 0)
     		{
     			$sql = "INSERT INTO `projectdocuments` (`projectID`, `documentID`, `documentDeadline`) 
@@ -33,16 +40,18 @@
 
     			if ($conn->query($sql) === true) 
     			{
-    				array_push($errorArray, "New record created successfully<br>");
+    				array_push($errorArray, "<span class='card-title red-text text-accent-4'><h3>De document is toegevoegd aan de project.</span></h3>");
     			}
     			else 
     			{
-    				echo "Error: " . $sql . "<br>" . $conn->error;
+    				array_push($errorArray, "<span class='card-title red-text text-accent-4'><h3>Er is iets fout gegaan bij het koppelen van de document</span></h3>");
+                    //echo "Error: " . $sql . "<br>" . $conn->error;
     			}
 
-    			$result->close();
-    			$conn->close();
-            	array_push($errorArray, "is gelukt<br>");
+                /* close result set */
+                $result->close();
+                /* close connection */
+                $conn->close();
 
             	return $errorArray;
     		}
@@ -51,7 +60,7 @@
            	 	/* close connection */
             	$conn->close();
           	 	// header('Location:login.php?status=fail');
-            	array_push($errorArray, "FAIL is al eerder opgegeven<br>");
+            	array_push($errorArray, "<span class='card-title red-text text-accent-4'><h3>De geselecteerde document is al gelinked.</span></h3>");
             	return $errorArray;
         	}
     	}
