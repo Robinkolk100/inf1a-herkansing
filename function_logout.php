2c11ad7160711fca2689a39a8fc4ac15f159da75
IvarHuizing <ivar.huizing@student.stenden.com>
2018-07-16 20:04:55 +0200

    add session_unset and session_destroy

--- function_logout.php
+++ function_logout.php
@@ -2,7 +2,8 @@
 
     // Stopping session to logout the user
     session_start();
-   unset($_SESSION['user']);
+    session_unset();
+    session_destroy();
     header('location: Index.php');
     echo "<span class='card-title lime-text text-accent-4'><h3>U bent uit gelogt</h3></span>";
 
