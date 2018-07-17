8aad428ff318aa8ef43cd132e697e79673b9f10c
WackyRage <a.m.westerhof@gmail.com>
2018-07-17 11:21:49 +0200

    .

--- home.php
+++ home.php
@@ -12,8 +12,6 @@ include 'function_addProject.php';
 include 'function_addProjectMember.php';
 include 'function_addProjectDocument.php';
 include 'function_warningProjectMember.php';
-include 'function_uploadProjectDocument.php';
-include 'function_updateDeadlineProjectDocument.php';
 	//include 'function_getProjects.php';
 include 'function_getProjectMembers.php';
 include 'function_getUsers.php';
@@ -360,11 +358,11 @@ include 'function_getAdviceTips.php';
 											</div>
 											<div class="col s4">
 												<strong>Deadline: </strong>
-												<input type="date" name="NewDeadline" value=<?php echo $document['documentDeadline']; ?>>
+												<input type="date" name="Deadline" value=<?php echo $document['documentDeadline']; ?>>
 											</div>
 											<div class="col s3">
-												<input type="submit" name="NewDeadline" class="waves-effect waves-light red btn" value="Aanpassen">
-												<input type="submit" name="Inleveren" class="waves-effect waves-light red btn" value=" Inleveren ">
+												<input type="submit" class="waves-effect waves-light red btn" value="Aanpassen">
+												<input type="submit" name="inleveren" class="waves-effect waves-light red btn" value=" Inleveren ">
 											</div>
 										</form>
 										<div id="modal<?php echo $document['documentID']; ?>" class="modal">
@@ -495,13 +493,6 @@ include 'function_getAdviceTips.php';
   } else {
     echo "geen project geselect";
   }
-
-												if (isset($_POST['NewDeadline'])) { 
-													updateProjectDocument($projectID, $document['documentID'], $_POST['NewDeadline']);
-												}
-												if (isset($_POST['Inleveren'])) {
-													uploadProjectDocument($projectID, $document['documentID']) ;
-												}
   ?>
 				<!--card widgets start-->
 				<div id="card-widgets">
